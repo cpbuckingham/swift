6fde7d3ddfc8c5175b2a671187d0ab526ca042d0
Cameron <cameron.p.buckingham@gmail.com>
2015-11-13 11:37:32 -0700

    hello world

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,5 +1,20 @@
-//: Playground - noun: a place where people can play
+var str = "hello world"
+str
+//no class needed to init, very simliar to JS
+//type inference will not be needed, compiler will do the work
 
-import UIKit
+var strVar:String = "something"
+//its rare that you need the type annotations
+//swift compiler does all the work
 
-var str = "Hello, playground"
+var int = 5
+int-int
+
+var someMessage:String
+someMessage = "hello"
+someMessage = 15
+
+//errors b/c it cannot convert
+
+
+
