cf1a8e8f2d71efccaa14bbd1d137d48a8a3f4cb9
Cameron <cameron.p.buckingham@gmail.com>
2015-11-13 12:12:00 -0700

    for loop

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,21 +1,4 @@
-for index in 1...5{
-    print(index)
+var i:Int
+for i = 0; i <= 10; ++i{
+    print(i)
 }
-
-//index acts as a constant, but if you dont need it as a constant, but put an underscore
-
-for _ in 1...5{
-    print("hi")
-}
-
-// _ means I don't need that index
-
-let base = 3
-let power = 10
-var answer = 1
-
-for _ in 1...power{
-    answer *= base
-}
-answer
-
