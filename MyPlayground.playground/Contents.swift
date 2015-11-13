7ae91736d88eba8deb9c20443f464c9545d7613e
Cameron <cameron.p.buckingham@gmail.com>
2015-11-13 11:39:44 -0700

    let vs var

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,20 +1,8 @@
-var str = "hello world"
-str
-//no class needed to init, very simliar to JS
-//type inference will not be needed, compiler will do the work
+let someConstant = "I am forever constant"
+//var - variable, let - constant
+someConstant = "lets modify it"
+//errors come 
 
-var strVar:String = "something"
-//its rare that you need the type annotations
-//swift compiler does all the work
-
-var int = 5
-int-int
-
-var someMessage:String
-someMessage = "hello"
-someMessage = 15
-
-//errors b/c it cannot convert
 
 
 
