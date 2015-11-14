012a802c236aa163370835d0cce96e0d4544a090
Cameron <cameron.p.buckingham@gmail.com>
2015-11-14 10:24:56 -0700

    where

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,14 +1,12 @@
 
-//tupal - it has a list of things
 
 let somePoint = (2,0)
 
 
 switch somePoint {
-case (let x,let y):
-    print("It was low \(x) \(y)")
-case (0,1):
-    print("It was 8")
+case (let x, 0)
+    where x !=2:
+    print("It was low \(x)")
 case (_,0):
     print("It was 9,10")
 case (-2...2, -2...2):
