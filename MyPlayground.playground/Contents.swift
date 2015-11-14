1ad38fccf2cd3a9427dc95b33fc1eaf82079a815
Cameron <cameron.p.buckingham@gmail.com>
2015-11-14 10:06:52 -0700

    if / else

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,4 +1,17 @@
+//if statements
+
 var i:Int
-for i = 0; i <= 10; ++i{
-    print(i)
+for i = 0; i < 10; ++i{
+    i
 }
+
+i
+
+if i < 10 {
+    print("yes it is")
+    
+}else if(i < 20){
+    print("Yes it is less than 20")
+    
+}
+
