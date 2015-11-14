de99fa81669f1f5e5e254a7871f89d8c027f7503
Cameron <cameron.p.buckingham@gmail.com>
2015-11-14 10:12:34 -0700

    switch with a range

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,18 +1,16 @@
-//if statements
 
-var i:Int
-for i = 0; i < 10; ++i{
-    i
-}
+let count = 3_000_000
 
 
-switch i {
+switch count {
 case 1,2,3:
     print("It was low")
 case 8:
     print("It was 8")
 case 9,10:
     print("It was 9,10")
+case 2_000_000...4_000_000:
+    print("it was between")
 default:
     print("It was none of the above")
 }
