f712dac431975073639a10873933415e84d3edee
Cameron <cameron.p.buckingham@gmail.com>
2015-11-14 10:30:53 -0700

    fallthrough

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,24 +1,24 @@
-//
-//
-//let somePoint = (2,0)
-//
-//
-//switch somePoint {
-//case (let x, 0)
-//    where x !=2:
-//    print("It was low \(x)")
-//case (_,0):
-//    print("It was 9,10")
-//case (-2...2, -2...2):
-//    print("it was between")
-//default:
-//    print("It was none of the above")
-//}
 
-var i:Int
-for i = 0; i < 10; ++i{
-    if i == 2{
-        continue
-    }
-        print(i)
+
+let somePoint = (2,0)
+
+
+switch somePoint {
+case (2, 0):
+    print("2,0")
+    fallthrough
+case (_,0):
+    print("It was 9,10")
+case (-2...2, -2...2):
+    print("it was between")
+default:
+    print("It was none of the above")
 }
+
+//var i:Int
+//for i = 0; i < 10; ++i{
+//    if i == 2{
+//        continue
+//    }
+//        print(i)
+//}
