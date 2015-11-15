ba797db894939a94e6e8bc482bf0bd0e92031107
Cameron <cameron.p.buckingham@gmail.com>
2015-11-15 10:04:14 -0700

    enumerate:

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -21,3 +21,9 @@ stringArray[1...3]
 for num in 1...3{
     print(num)
 }
+
+stringArray[2]
+stringArray.removeLast()
+
+for(index, value) in enum(stringArray)
+print("at\(index+1) is this:\(value)")
