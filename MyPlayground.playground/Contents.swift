4b13392fc41b3322b40d0bb6b1741c9aa06fe579
Cameron <cameron.p.buckingham@gmail.com>
2015-11-15 09:57:53 -0700

    arrays

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,21 +1,10 @@
-var i:Int
-let somePoint = (2,0)
-mainLoop: for i = 0; i < 10; ++i{
-    if i == 2{
-        continue
-    }
+var stringArray:[String]=["hello","there","sir"]
 
+stringArray
 
-    mainSwitch: switch somePoint {
-case (2, 0):
-    print("2,0")
-//        break mainLoop
-        break mainSwitch
-case (_,0):
-    print("It was 9,10")
-case (-2...2, -2...2):
-    print("it was between")
-default:
-    print("It was none of the above")
-}
-}
+stringArray += ["how"]
+stringArray.append("are")
+stringArray.append("you")
+stringArray
+stringArray.insert("its", atIndex: 0)
+stringArray
