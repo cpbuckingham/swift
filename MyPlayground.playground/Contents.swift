0d350522b6dc86c0eae04e893560c2158e6fe697
Cameron <cameron.p.buckingham@gmail.com>
2015-11-18 08:42:13 -0700

    empty array

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,29 +1,8 @@
-var stringArray:[String]=["hello","there","sir"]
+var someInt:[Int]=[]
 
-stringArray
+someInt.append(3)
+someInt.count
+someInt=[]
+someInt.count
 
-stringArray += ["how"]
-stringArray.append("are")
-stringArray.append("you")
-stringArray
-stringArray.insert("its", atIndex: 0)
-stringArray
 
-for word in stringArray{
-    print(word)
-}
-stringArray.removeAtIndex((3))
-
-stringArray
-
-stringArray[1...3]
-
-for num in 1...3{
-    print(num)
-}
-
-stringArray[2]
-stringArray.removeLast()
-
-for(index, value) in enum(stringArray)
-print("at\(index+1) is this:\(value)")
